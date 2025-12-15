7d2fa3f56a4d3f9bdc109f725cb7a1be75f5e31b
mdshap <notunmdshaptarshi@gmail.com>
2025-12-15 17:38:40 +0600

    stripe added

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const Stripe = require("stripe");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://ScholarshipStream:T6ClGatm1mQQp6cQ@cluster0.blss57h.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,13 +21,37 @@ const client = new MongoClient(uri, {
   },
 });
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const { amount } = req.body;
+
+    if (!amount) {
+      return res.status(400).send({ message: "Amount is required" });
+    }
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: "usd",
+      payment_method_types: ["card"],
+    });
+
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    console.error("Stripe Error:", error.message);
+    res.status(500).send({ error: error.message });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Server is Available");
 });
 
 async function run() {
   try {
-    //CONNECT TO MONGODB
     await client.connect();
     console.log("Connected");
 
@@ -87,7 +113,6 @@ async function run() {
 
     //SCHOLARSHIP RELATED APIs
     const scholarshipsCollection = db.collection("Scholarships");
-
     app.post("/scholarships", async (req, res) => {
       const newScholarship = req.body;
       newScholarship.createdAt = new Date();
@@ -111,8 +136,8 @@ async function run() {
         query.scholarshipCategory = category;
       }
 
-      const scholarships = scholarshipsCollection.find(query);
-      const result = await scholarships.toArray();
+      const filteredScholarships = scholarshipsCollection.find(query);
+      const result = await filteredScholarships.toArray();
 
       if (order === "ascending") {
         result.sort(
@@ -126,8 +151,98 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const filteredScholarship = await scholarshipsCollection.findOne(query);
+      res.send(filteredScholarship);
+    });
+
+    app.patch("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: updatedData,
+      };
+
+      const result = await scholarshipsCollection.updateOne(filter, updateDoc);
+
+      if (result.matchedCount === 0) {
+        return res.status(404).send({ message: "Scholarship not found" });
+      }
+
+      res.send(result);
+    });
+
+    app.delete("/scholarships/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await scholarshipsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
     //APPLICATION RELATED APIs
     const applicationsCollection = db.collection("Applications");
+    app.post("/applications", async (req, res) => {
+      try {
+        const application = req.body;
+
+        if (
+          !application.scholarshipId ||
+          !application.userId ||
+          !application.userEmail
+        ) {
+          return res.status(400).send({ message: "Invalid application data" });
+        }
+
+        const newApplication = {
+          scholarshipId: application.scholarshipId,
+          userId: application.userId,
+          userName: application.userName,
+          userEmail: application.userEmail,
+
+          universityName: application.universityName,
+          scholarshipCategory: application.scholarshipCategory,
+          degree: application.degree,
+
+          applicationFees: application.applicationFees,
+          serviceCharge: application.serviceCharge,
+
+          applicationStatus: "pending",
+          paymentStatus: application.paymentStatus || "unpaid",
+
+          applicationDate: new Date(),
+          feedback: "",
+        };
+
+        const result = await applicationsCollection.insertOne(newApplication);
+
+        res.send({
+          success: true,
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        res.status(500).send({ message: error.message });
+      }
+    });
+
+    app.get("/applications", async (req, res) => {
+      const applications = applicationsCollection.find();
+      const result = await applications.toArray();
+      res.send(result);
+    });
+
+    app.get("/applications/:userID", async (req, res) => {
+      const userID = req.params.userID;
+      const query = { userId: userID };
+      const filteredApplication = applicationsCollection.find(query);
+      const result = await filteredApplication.toArray()
+      res.send(result);
+    });
 
     //REVIEWS RELATED APIs
     const reviewsCollection = db.collection("Reviews");
